82ad215a7096b359c8bffa59415840b1ed801f63
Luan Vilela <luan.lopes@ufms.br>
2025-12-15 00:27:37 -0400

    start project

---
+++ src/app/_layout.tsx
@@ -0,0 +1,31 @@
+import { Loading } from "@/components/ui/loadings/loading";
+import "@/styles/global.css";
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      <Slot />
+    </GestureHandlerRootView>
+  );
+}
